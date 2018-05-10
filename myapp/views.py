6024de38fe2a78ebb34944521a57347843399cd8
Kensuke Numakura <knmkr3gma@gmail.com>
2018-05-10 16:11:40 +0900

    call enterprise API by SDK

--- myapp/views.py
+++ myapp/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.conf import settings
-from .client import *
+import genomelink
 
 
 def index(request):
     if request.method == 'POST':
-        # you'll acquire an access token issued per user as POST parameter
-        # after they have successfully uploaded their DNA data via popup window.
+        # You'll acquire an access token issued per user as a POST parameter.
         token = request.POST.get('genomelinkToken')
 
-        # for this example app, if token is valid,
-        #
-        # ```
-        # {'detail': 'Hello GENOME LINK!'}
-        # ```
-        #
-        # will be returnd as JSON.
         try:
-            data = Report.fetch(client_secret=settings.GENOMELINK_CLIENT_SECRET, token=token)
-            messages.success(request, data['detail'])
-        except GenomeLinkError as e:
+            # Then, you can fetch reports by using the token and your GENOMELINK_CLIENT_SECRET.
+            reports = genomelink.Report.fetch(name='example', population='european', token=token, client_secret=settings.GENOMELINK_CLIENT_SECRET)
+
+            # Now you can display the reports.
+            # In real applications, you should store report data into persistent storage.
+            for report in reports:
+                messages.success(request, '{}: {}'.format(report.phenotype['display_name'], report.summary['text']))
+
+        except genomelink.errors.GenomeLinkError as e:
+            # When something is wrong with API request, API client raise errors.
+            # In real applications, you should not display direct error messages to users.
             messages.warning(request, e)
 
     return render(request, 'myapp/index.html', {'GENOMELINK_CLIENT_ID': settings.GENOMELINK_CLIENT_ID})
