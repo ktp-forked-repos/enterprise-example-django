09fd8d8bef556852bddfa225bcb12f67a680d23f
Kensuke Numakura <knmkr3gma@gmail.com>
2018-07-10 21:48:28 +0900

    add e2e testing

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
 
-# Create your tests here.
+
+class GenomelinkBrowserTestCase(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        # $ brew install chromedriver
+        super(GenomelinkBrowserTestCase, cls).setUpClass()
+        cls.browser = webdriver.Chrome()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super(GenomelinkBrowserTestCase, cls).tearDownClass()
+
+    def setUp(self):
+        pass
+
+    def test_submit_without_file_should_alert(self):
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_xpath('//button').click()
+
+        self.browser.switch_to_frame(self.browser.find_element_by_id('genomelinkFrame'))
+        self.browser.find_element_by_xpath('//button').click()
+
+        alert = self.browser.switch_to_alert()
+        self.assertEqual(alert.text, 'No file selected.')
+        alert.accept()
