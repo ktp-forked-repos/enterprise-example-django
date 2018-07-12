29fecd0ec59263eb1b3abd0f444a6372df3fb9a2
Kensuke Numakura <knmkr3gma@gmail.com>
2018-07-12 14:13:27 +0900

    add a radar chart

--- myapp/views.py
+++ myapp/views.py
@@ -14,11 +14,18 @@ def index(request):
         try:
             # Then, you can fetch reports by using the token and your GENOMELINK_CLIENT_SECRET.
             # In real applications, you should store report data into persistent storage.
-            reports = genomelink.Report.fetch(name='example', population='european', token=token, client_secret=settings.GENOMELINK_CLIENT_SECRET)
+            reports = genomelink.Report.fetch(token=token,
+                                              client_secret=settings.GENOMELINK_CLIENT_SECRET,
+                                              name='foo',             # TODO: remove
+                                              population='european')  # TODO: remove
             messages.success(request, 'Success!')
         except genomelink.errors.GenomeLinkError as e:
             # When something is wrong with API request, API client raise errors.
             # In real applications, you should not display direct error messages to users.
             messages.warning(request, e)
 
-    return render(request, 'myapp/index.html', {'GENOMELINK_CLIENT_ID': settings.GENOMELINK_CLIENT_ID, 'reports': reports})
+    context = {
+        'GENOMELINK_CLIENT_ID': settings.GENOMELINK_CLIENT_ID,
+        'reports': reports
+    }
+    return render(request, 'myapp/index.html', context)
